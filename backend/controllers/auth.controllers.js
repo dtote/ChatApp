1e57c8169efb01a00f600a938ab18304077aa0f1
SamLorenzoSanc <samuellorenzosanchez@gmail.com>
2025-02-19 14:05:10 +0000

    l

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -193,13 +193,13 @@ export const loginFacial = async (req, res) => {
 
     // Crear descriptores etiquetados
     const labeledDescriptors = users.map(user => {
-      if (!user.faceDescriptor || !Array.isArray(user.faceDescriptor[0])) {
+      if (!user.faceDescriptor || !Array.isArray(user.faceDescriptor) || !user.faceDescriptor[0].values || !Array.isArray(user.faceDescriptor[0].values)) {
         console.error(`Invalid descriptor for user: ${user._id}`);
         return null;
       }
       return new faceapi.LabeledFaceDescriptors(
         user._id.toString(), // Usa el ID del usuario como etiqueta
-        [new Float32Array(Object.values(user.faceDescriptor[0]))]
+        [new Float32Array(user.faceDescriptor[0].values)] // Asumiendo que 'values' es el array que necesitas
       );
     }).filter(desc => desc !== null);
 
