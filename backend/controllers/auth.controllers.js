130802d22a0b80de580a2b507943e18f524e4502
SamLorenzoSanc <samuellorenzosanchez@gmail.com>
2025-02-19 00:13:51 +0000

    Changes

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -178,7 +178,7 @@ export const loginFacial = async (req, res) => {
     }
 
     // Convertir el descriptor facial de JSON a Float32Array
-    const inputDescriptor = new Float32Array(JSON.parse(faceDescriptor));
+    const inputDescriptor = JSON.parse(faceDescriptor);
 
     console.log('Input descriptor:', inputDescriptor);
     // Obtener todos los usuarios de la base de datos
