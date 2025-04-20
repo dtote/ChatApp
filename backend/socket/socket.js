e0875651955c3fc40b76d4e26708653596c906cf
SamLorenzoSanc <samuellorenzosanchez@gmail.com>
2025-04-20 11:11:22 +0100

    Fix build 3

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -11,7 +11,7 @@ let io;
 export const initializeSocket = (httpServer) => {
   io = new Server(httpServer, {
     cors: {
-      origin: ["http://localhost:3000"],
+      origin: ["https://chatapp-1-eebi.onrender.com"],
       methods: ["GET", "POST"],
       credentials: true,
       transports: ["websocket"],
