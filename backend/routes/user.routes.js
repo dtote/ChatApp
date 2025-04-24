b5d80a049167a7823bb0d45a49593e11b5b5eb4c
SamLorenzoSanc <samuellorenzosanchez@gmail.com>
2025-04-24 21:14:02 +0100

    Add Functionality to production

--- backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protectRoute from "../middleware/protectRoute.js";
-import  {getUserforSidebars, getUserProfilePic, getUserPopupData} from "../controllers/user.controllers.js";
-const router = express.Router();
-
-router.get("/", protectRoute, getUserforSidebars);
-router.get('/:userId/profile-pic', getUserProfilePic);
-router.get('/:userId/popup-data', getUserPopupData);
-export default router;
--- frontend/src/zustand/useSecurity.js
+++
@@ -1,8 +0,0 @@
-import {create} from 'zustand';
-
-const useSecurity = create((set) => ({
-  selectedKeySize: 'ML-KEM-512', // Aquí almacenamos el tipo de clave
-  setSelectedKeySize: (keySize) => set({ selectedKeySize: keySize }), // Función para actualizar el tipo de clave
-}));
-
-export default useSecurity;
