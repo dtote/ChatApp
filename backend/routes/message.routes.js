74fddab9b51d3fbc4e9c9bd42fff7f68cbf0134c
SamLorenzoSanc <samuellorenzosanchez@gmail.com>
2025-04-25 00:09:09 +0100

    Add Cloudinary

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -3,18 +3,19 @@ import {sendMessage, getMessages, reactMessage} from '../controllers/message.con
 import multer from "multer";
 import {protectRoute} from '../middleware/protectRoute.js';
 import path from 'path';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../utils/cloudinary.js';
 
-// Configuración de multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'chat_uploads',
+    resource_type: 'auto',
+    allowed_formats: ['jpg', 'png', 'pdf', 'mp4'], 
+    public_id: (req, file) => file.fieldname + '-' + Date.now(),
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const extension = path.extname(file.originalname); 
-    cb(null, file.fieldname + '-' + uniqueSuffix + extension);
-  }
 });
+
 const upload = multer({ storage: storage });
 
 const router = express.Router();
