65e7aa233b78607980a970f973f8a815c44b57a6
Nestor Torres <neeestor08@gmail.com>
2025-07-17 12:04:40 +0100

    feat(cloudinary): updated cloudinary config

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 export default cloudinary;
